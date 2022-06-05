c93c31f4cdfe4b939d52e266c8c56193e5701f07
m5adda <106909308+m5adda@users.noreply.github.com>
2022-06-05 11:23:52 +0100

    First upload of the game

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 30;
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+}
---
+++ Assets/scripts/garbage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First : MonoBehaviour
+{
+    int x = 5;
+    int y = 2;
+    string username = "bachra";
+    void Start()
+    {
+        Debug.Log(x);
+        Debug.Log(y);
+        int z = x * y;
+        Debug.Log(z);
+        y = y + 1;
+        Debug.Log("HELLO " + username + " you have " + x + " lives");
+        Debug.Log(5==3);
+        Debug.Log(1 < 2);
+        Debug.Log(2 < 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Debug.Log(x);
+    }
+}
