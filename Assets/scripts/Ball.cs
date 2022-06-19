92203c11012c3327770add3426f261392d3a1f6a
m5adda <106909308+m5adda@users.noreply.github.com>
2022-06-19 10:00:54 +0100

    PongClone.2
    Better Version of this game

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -1,11 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class Ball : MonoBehaviour
 {
+    public GameObject scorerightText;
+    public GameObject scoreleftText;
+    int scoreRight;
+    int scoreLeft;
     public float speed = 30;
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        
+    }
+    float hitFactor (Vector2 ballPos,Vector2 racketPos,float racketHeight)
+    {
+        // || 1 <- at the top of the racket
+        // ||
+        // || 0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket 
+        return (ballPos.y - racketPos.y) / racketHeight; 
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Wallright")
+        {
+            //this line will just add 1 point to the score
+            scoreRight++;
+            scorerightText.GetComponent<TextMeshProUGUI>().text = scoreRight.ToString();
+        }
+        if (col.gameObject.name == "Wallleft")
+        {
+            scoreLeft++;
+            scoreleftText.GetComponent<TextMeshProUGUI>().text = scoreLeft.ToString();
+        }
+        {
+            // Note: 'col' holds the collision information. If the
+            // Ball collided with a racket, then:
+            //   col.gameObject is the racket
+            //   col.transform.position is the racket's position
+            //   col.collider is the racket's collider
+
+            // Hit the left Racket?
+            if (col.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "RacketRight")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+
+
+            }
+        }
     }
 }
---
+++ Assets/scripts/checkpoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoint : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(Sum(4,5)) ;
+        Debug.Log(Prod(7, 9));
+        Debug.Log(divi(5, 2));
+    }
+
+    void Update()
+    {
+        
+    }
+
+    float Sum(float a, float b)
+    {
+        return a + b;
+    }
+    float Prod(float a,float b)
+    {
+        return a * b;
+     }
+    
+    float divi(float a,float b)
+    {
+        if (b == 0){
+            Debug.Log("ERROR");
+        }
+        else
+        {
+            return a / b;
+        }
+
+        return 0;
+    }
+}
